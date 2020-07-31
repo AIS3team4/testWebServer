9e676c2cb2de25f45365f849f6ac7c6e715a88e2
che30122 <che30122@gmail.com>
2020-07-31 12:24:02 -0700

    che change docker-compose_1.yml

--- .github/actions/container/DAST/zap/Start_1.sh
+++ .github/actions/container/DAST/zap/Start_1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export
 ls -al /
-echo $WEB_IP
-/zap/zap-baseline.py -t http://$WEB_IP:$WEB_PORT > /tmp/testreport.html
+echo $WEB_URL
+/zap/zap-baseline.py -t http://$WEB_URL > /tmp/testreport.html
 python3 $CHECKER/check.py /tmp/testreport.html
