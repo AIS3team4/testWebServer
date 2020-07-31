0e0321017487ab1173c21357a8bd7e3758037a38
cue08056 <cue08056@gmail.com>
2020-07-31 17:31:09 +0800

    t

--- .github/actions/container/UniteTest/UniteTest.sh
+++ .github/actions/container/UniteTest/UniteTest.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-PATH=/bin:/usr/bin
-/usr/bin/make test
-/usr/bin/file webserver_fork > ./result.txt
-/usr/share/gcc-8/python checker/check.py ./result.txt
+make test
+file webserver_fork > ./result.txt
+python $CHECKER/check.py ./result.txt
