4d13b34464990535fcab14c6ca7ed9155253602e
cue08056 <cue08056@gmail.com>
2020-07-31 17:24:44 +0800

    t

--- .github/actions/container/UniteTest/UniteTest.sh
+++ .github/actions/container/UniteTest/UniteTest.sh
@@ -2,4 +2,4 @@
 PATH=/bin:/usr/bin
 /usr/bin/make test
 /usr/bin/file webserver_fork > ./result.txt
-/usr/bin/python3 checker/check.py ./result.txt
+/usr/share/gcc-8/python checker/check.py ./result.txt
