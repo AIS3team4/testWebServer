046896d73ed5eb5c5396333c11e824343d7ebd6e
cue08056 <cue08056@gmail.com>
2020-07-31 23:33:05 +0800

    t

--- .github/actions/container/DAST/zap/Start.sh
+++ .github/actions/container/DAST/zap/Start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 export
 ls
-find / -name zap-baseline.py
-zap-baseline.py -t http://$WEB_IP:$WEB_PORT -r testreport.html
+/zap/zap-baseline.py -t http://$WEB_IP:$WEB_PORT -r testreport.html
 python3 $CHECKER/check.py testreport.html
